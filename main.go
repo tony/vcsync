642796198c0c19ebf9bacc49ea541bf8c6b4438b
Tony Narlock <tony@git-pull.com>
2016-03-07 06:46:52 -0600

    wip interim trying to parse varying configs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"reflect"
 
 	"github.com/Masterminds/vcs"
 	"github.com/spf13/pflag"
@@ -19,18 +20,48 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	m := map[string]map[string]interface{}{}
 	dirs := viper.AllKeys()
 	repos := []vcs.Repo{}
 	for _, x := range dirs {
-		m[x] = viper.GetStringMap(x)
-		ExpandConfig(x, m[x], &repos)
+
+		v := viper.Sub(x)
+		for _, k := range v.AllKeys() {
+			fmt.Printf("%T\n", k)
+			ExpandConfig(x, k, v.Get(k), &repos, v)
+		}
 	}
 }
 
-func ExpandConfig(dir string, repositories map[string]interface{}, repos *[]vcs.Repo) {
+type verboseRepo struct {
+	repo    string
+	remotes map[string]interface{}
+}
+
+func castToMapStringInterface(
+	src map[interface{}]interface{}) map[string]interface{} {
+	tgt := map[string]interface{}{}
+	for k, v := range src {
+		tgt[fmt.Sprintf("%v", k)] = v
+	}
+	return tgt
+}
+
+func ExpandConfig(dir string, name string, repoinfo interface{}, repos *[]vcs.Repo, v *viper.Viper) {
 	fmt.Println(dir)
-	for name, repo := range repositories {
-		fmt.Println(name, repo)
+
+	switch repoinfo.(type) {
+	case string:
+	//
+	case map[interface{}]interface{}:
+		//
+		fmt.Printf("%v\t: %v<%s>\n", name, repoinfo, reflect.TypeOf(repoinfo))
+		vv := castToMapStringInterface(repoinfo.(map[interface{}]interface{}))
+		// vv := v.Sub(name)
+		fmt.Printf("created a sub sub: %v<%T>", vv, vv)
+	case nil:
+		return
+	}
+	if reflect.TypeOf(repoinfo).Kind() == reflect.Map {
+		fmt.Printf("PMG ITS A MAP")
 	}
 }
