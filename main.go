c9884eb5888ecbfcd4eb5b118b667022d478f8a8
Tony Narlock <tony@git-pull.com>
2016-03-09 08:42:38 -0600

    detect repos in more yaml styles from vcspull

--- main.go
+++ main.go
@@ -15,49 +15,59 @@ func main() {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 	m := map[string]map[string]interface{}{}
-	repos := []vcs.Repo{}
+	var repos []vcs.Repo
+	fmt.Println(repos)
+	var legacyRepos []LegacyRepoConf
 	for _, x := range viper.AllKeys() {
 		m[x] = viper.GetStringMap(x)
-		ExpandConfig(x, m[x], &repos, viper.Sub(x))
+		ExpandConfig(x, m[x], &legacyRepos, viper.Sub(x))
+		fmt.Println(legacyRepos[len(legacyRepos)-1:][0])
 	}
 }
 
-type RepoConf struct {
+type LegacyRepoConf struct {
 	name    string
 	url     string
 	path    string
 	remotes map[string]string
 }
 
-func ExpandConfig(dir string, entries map[string]interface{}, repos *[]vcs.Repo, v *viper.Viper) {
+func ExpandConfig(dir string, entries map[string]interface{}, repos *[]LegacyRepoConf, v *viper.Viper) {
 	//fmt.Println(dir)
 	for name, repo := range entries {
 		// fmt.Printf("name: %v\t repo: %v\n", name, repo)
 		switch repo.(type) {
 		case string:
-			repoConf := RepoConf{
+			*repos = append(*repos, LegacyRepoConf{
 				name:    name,
 				url:     repo.(string),
 				path:    dir,
 				remotes: nil,
-			}
-			fmt.Println(repoConf)
+			})
 		case map[interface{}]interface{}:
 			r := castToMapStringInterface(repo.(map[interface{}]interface{}))
 			if r["remotes"] != nil {
-				for remote_name, remote := range r["remotes"].(map[interface{}]interface{}) {
-					fmt.Println(RepoConf{
+				for remote_name, remote := range castToMapStringString(r["remotes"].(map[interface{}]interface{})) {
+					*repos = append(*repos, LegacyRepoConf{
 						name: name,
 						path: dir,
 						url:  r["repo"].(string),
 						remotes: map[string]string{
-							remote_name.(string): remote.(string),
+							remote_name: remote,
 						},
 					})
 				}
+			} else {
+				*repos = append(*repos, LegacyRepoConf{
+					name:    name,
+					path:    dir,
+					url:     r["repo"].(string),
+					remotes: nil,
+				})
+				fmt.Printf("No remotes detected, check your formatting for %s at %s", name, repo)
 			}
 		default:
-			// fmt.Printf("name %v: verbose repo (type %T)\n", name, repo)
+			fmt.Printf("name %v: verbose repo (type %T)\n", name, repo)
 		}
 	}
 }
@@ -66,6 +76,14 @@ func castToMapStringInterface(src map[interface{}]interface{}) map[string]interf
 	tgt := map[string]interface{}{}
 	for k, v := range src {
 		tgt[fmt.Sprintf("%v", k)] = v
+	}
+	return tgt
+}
+
+func castToMapStringString(src map[interface{}]interface{}) map[string]string {
+	tgt := make(map[string]string)
+	for k, v := range src {
+		tgt[fmt.Sprintf("%v", k)] = v.(string)
 	}
 	return tgt
 }
