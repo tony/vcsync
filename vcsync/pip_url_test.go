98a4ba41ba263efe3a6fbad211079a2c73693da7
Tony Narlock <tony@git-pull.com>
2016-03-11 06:48:07 -0600

    Add some more test cases

--- vcsync/pip_url_test.go
+++ vcsync/pip_url_test.go
@@ -9,6 +9,9 @@ func TestFindsGit(t *testing.T) {
 		vcs string
 	}{
 		{"git+https://github.com/tony/.dot-configs", "git"},
+		{"git+ssh://git@github.com/tony/roundup.git", "git"},
+		{"hg+http://foicica.com/hg/textadept", "hg"},
+		{"svn+http://svn.code.sf.net/p/docutils/code/trunk", "svn"},
 	}
 
 	for _, tt := range configTests {
