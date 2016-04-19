22b39e21ce5f3e43b6ff512fd3540d8e3ac09cff
Tony Narlock <tony@git-pull.com>
2016-04-19 14:33:02 -0500

    Move repo specific tests to repo_test, add config tests

--- vcsync/config_test.go
+++ vcsync/config_test.go
@@ -1,128 +1,38 @@
 package vcsync_test
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
 	"testing"
 
 	"github.com/tony/vcs"
 	"github.com/tony/vcsync/vcsync"
 )
 
-func TestRepo(t *testing.T) {
-	var configTests = []struct {
-		url      string
-		location string
-		vtype    vcs.Type
-	}{
-		{"git+https://github.com/tony/.dot-configs@moo", "https://github.com/tony/.dot-configs", vcs.Git},
-		{"git+ssh://git@github.com/tony/roundup.git@master", "ssh://git@github.com/tony/roundup.git", vcs.Git},
-		{"hg+http://foicica.com/hg/textadept@ha", "http://foicica.com/hg/textadept", vcs.Hg},
-		{"svn+http://svn.code.sf.net/p/docutils/code/trunk@2019", "http://svn.code.sf.net/p/docutils/code/trunk", vcs.Svn},
+func TestLoadRepo(t *testing.T) {
+	var r vcsync.Repos
+	r.LoadRepos("dir", map[string]interface{}{"myrepo": "git+https://github.com/moo/moo"})
+
+	if len(r) != 1 {
+		t.Errorf("loaded %v instead of one", len(r))
 	}
-
-	for _, tb := range configTests {
-		vURL, err := vcsync.ParsePipURL(tb.url)
-		if err != nil {
-			t.Error(err)
-		}
-		repo, err := vcsync.NewRepo(vURL.Vtype, vURL.Location(), "./testhgrepo")
-
-		if err != nil {
-			t.Error(err)
-		}
-		if repo.Vcs() != tb.vtype {
-			t.Errorf("vcs should resolve to %s, got: %v", tb.location, vURL.Location())
-		}
+	if r[0].Repo.Vcs() != vcs.Git {
+		t.Errorf("should return correct repo vcs type, found %v", r[0].Repo.Vcs())
 	}
 }
 
-func TestGitAddRemote(t *testing.T) {
-	new_repo_remote := "https://github.com/lol/lol"
-	second_remote := "https://github.com/wut/wut"
-	tempDir, err := ioutil.TempDir("", "go-vcs-tests")
-	if err != nil {
-		t.Error(err)
+func TestLoadExpanded(t *testing.T) {
+	var r vcsync.Repos
+	r.LoadRepos(
+		"dir",
+		map[string]interface{}{"myrepo": map[interface{}]interface{}{
+			"repo": "hg+https://github.com/moo/moo",
+		},
+		},
+	)
+
+	if len(r) != 1 {
+		t.Errorf("loaded %v instead of one", len(r))
 	}
-	defer func() {
-		err = os.RemoveAll(tempDir)
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-
-	repo, err := vcs.NewGitRepo(tempDir+"/VCSTestRemote", tempDir+"/VCSTestRepo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = repo.Init(); err != nil {
-		t.Fatal(err)
-	}
-
-	if _, err = vcsync.AddRemote(repo, "origin", new_repo_remote); err != nil {
-		t.Error(err)
-	}
-
-	out, err := repo.RunFromDir("git", "remote", "-v")
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(string(out), new_repo_remote) {
-		t.Errorf("vcs should add remote properly, %s not found in %s", new_repo_remote, string(out))
-	}
-
-	// Adding a remote already exists
-	if _, err = vcsync.AddRemote(repo, "origin", second_remote); err != nil {
-		if !strings.Contains(err.Error(), fmt.Sprintf("remote %s already exists.", "origin")) {
-			t.Error("Adding a remote if one exist should raise an error")
-		}
-	}
-}
-
-func TestGitUpdateRemote(t *testing.T) {
-	second_remote := "https://github.com/wut/wut"
-	tempDir, err := ioutil.TempDir("", "go-vcs-tests")
-	if err != nil {
-		t.Error(err)
-	}
-	defer func() {
-		err = os.RemoveAll(tempDir)
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-
-	repo, err := vcs.NewGitRepo(tempDir+"/VCSTestRemote", tempDir+"/VCSTestRepo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = repo.Init(); err != nil {
-		t.Fatal(err)
-	}
-
-	new_repo_remote := "https://github.com/lol/lol"
-
-	if _, err = vcsync.AddRemote(repo, "origin", new_repo_remote); err != nil {
-		t.Error(err)
-	}
-
-	// Update a repo
-	if _, err = vcsync.UpdateRemote(repo, "origin", second_remote); err != nil {
-		t.Error(err)
-	}
-
-	out, err := repo.RunFromDir("git", "remote", "-v")
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(string(out), second_remote) {
-		t.Errorf("vcs should update remote properly, %s not found in %s", second_remote, string(out))
+	if r[0].Repo.Vcs() != vcs.Hg {
+		t.Errorf("should return correct repo vcs type, found %v", r[0].Repo.Vcs())
 	}
 }
---
+++ vcsync/repo_test.go
@@ -0,0 +1,128 @@
+package vcsync_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/tony/vcs"
+	"github.com/tony/vcsync/vcsync"
+)
+
+func TestRepo(t *testing.T) {
+	var configTests = []struct {
+		url      string
+		location string
+		vtype    vcs.Type
+	}{
+		{"git+https://github.com/tony/.dot-configs@moo", "https://github.com/tony/.dot-configs", vcs.Git},
+		{"git+ssh://git@github.com/tony/roundup.git@master", "ssh://git@github.com/tony/roundup.git", vcs.Git},
+		{"hg+http://foicica.com/hg/textadept@ha", "http://foicica.com/hg/textadept", vcs.Hg},
+		{"svn+http://svn.code.sf.net/p/docutils/code/trunk@2019", "http://svn.code.sf.net/p/docutils/code/trunk", vcs.Svn},
+	}
+
+	for _, tb := range configTests {
+		vURL, err := vcsync.ParsePipURL(tb.url)
+		if err != nil {
+			t.Error(err)
+		}
+		repo, err := vcsync.NewRepo(vURL.Vtype, vURL.Location(), "./testhgrepo")
+
+		if err != nil {
+			t.Error(err)
+		}
+		if repo.Vcs() != tb.vtype {
+			t.Errorf("vcs should resolve to %s, got: %v", tb.location, vURL.Location())
+		}
+	}
+}
+
+func TestGitAddRemote(t *testing.T) {
+	new_repo_remote := "https://github.com/lol/lol"
+	second_remote := "https://github.com/wut/wut"
+	tempDir, err := ioutil.TempDir("", "go-vcs-tests")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	repo, err := vcs.NewGitRepo(tempDir+"/VCSTestRemote", tempDir+"/VCSTestRepo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = repo.Init(); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = vcsync.AddRemote(repo, "origin", new_repo_remote); err != nil {
+		t.Error(err)
+	}
+
+	out, err := repo.RunFromDir("git", "remote", "-v")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !strings.Contains(string(out), new_repo_remote) {
+		t.Errorf("vcs should add remote properly, %s not found in %s", new_repo_remote, string(out))
+	}
+
+	// Adding a remote already exists
+	if _, err = vcsync.AddRemote(repo, "origin", second_remote); err != nil {
+		if !strings.Contains(err.Error(), fmt.Sprintf("remote %s already exists.", "origin")) {
+			t.Error("Adding a remote if one exist should raise an error")
+		}
+	}
+}
+
+func TestGitUpdateRemote(t *testing.T) {
+	second_remote := "https://github.com/wut/wut"
+	tempDir, err := ioutil.TempDir("", "go-vcs-tests")
+	if err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		err = os.RemoveAll(tempDir)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	repo, err := vcs.NewGitRepo(tempDir+"/VCSTestRemote", tempDir+"/VCSTestRepo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = repo.Init(); err != nil {
+		t.Fatal(err)
+	}
+
+	new_repo_remote := "https://github.com/lol/lol"
+
+	if _, err = vcsync.AddRemote(repo, "origin", new_repo_remote); err != nil {
+		t.Error(err)
+	}
+
+	// Update a repo
+	if _, err = vcsync.UpdateRemote(repo, "origin", second_remote); err != nil {
+		t.Error(err)
+	}
+
+	out, err := repo.RunFromDir("git", "remote", "-v")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !strings.Contains(string(out), second_remote) {
+		t.Errorf("vcs should update remote properly, %s not found in %s", second_remote, string(out))
+	}
+}
