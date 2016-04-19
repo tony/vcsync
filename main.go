c6d3ee3ce3e8f485ea595ca270bc20588da38459
Tony Narlock <tony@git-pull.com>
2016-04-19 12:04:11 -0500

    revert

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"github.com/tony/vcsync/commands"
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/tony/vcsync/vcsync"
 )
 
+var r vcsync.Repos
+
 func main() {
-	commands.Execute()
+	viper.SetConfigName(".vcspull")
+	viper.AddConfigPath("$HOME")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	m := map[string]map[string]interface{}{}
+
+	for _, x := range viper.AllKeys() {
+		m[x] = viper.GetStringMap(x)
+		r.LoadRepos(x, m[x])
+		log.Debug(r[len(r)-1:])
+	}
+
+	for _, repo := range r {
+		log.Infof("%s @ %s", repo.Repo.LocalPath(), repo.Repo.Remote())
+	}
+	log.Infof("%d repositories loaded.", len(r))
 }
